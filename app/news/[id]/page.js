727d288f927d7d1996b19cc4feb2665ea1cd066d
SivaSurender <e.surender14@gmail.com>
2024-12-31 12:20:39 +0530

    not found

--- app/news/[id]/page.js
+++ app/news/[id]/page.js
@@ -1,9 +1,24 @@
-function NewsType({ params }) {
+import { notFound } from "next/navigation";
+
+import { DUMMY_NEWS } from "@/dummy-new";
+
+export default function NewsType({ params }) {
+  console.log(params, "Dsdf");
+  const newsSlug = params.id;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+
+  if (!newsItem) {
+    notFound();
+  }
+
   return (
-    <>
-      <h1>News Page {params.id}</h1>
-    </>
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
   );
 }
-
-export default NewsType;
