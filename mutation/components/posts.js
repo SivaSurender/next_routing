572b00bbf02a1099c0c4e7ec38b5990e26bf11d0
bala_e <e.surender14@gmail.com>
2025-01-07 18:03:13 +0530

    Update PostForm label and implement like status toggle in Post component

--- mutation/components/posts.js
+++ mutation/components/posts.js
@@ -1,7 +1,12 @@
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
+import { updatePostLikeStatus } from "@/lib/posts";
 
 function Post({ post }) {
+  async function togglePostLikeStatus(postId, formData) {
+    "use server";
+    updatePostLikeStatus(postId, 2);
+  }
   return (
     <article className="post">
       <div className="post-image">
@@ -12,14 +17,19 @@ function Post({ post }) {
           <div>
             <h2>{post.title}</h2>
             <p>
-              Shared by {post.userFirstName} on{' '}
+              Shared by {post.userFirstName} on{" "}
               <time dateTime={post.createdAt}>
                 {formatDate(post.createdAt)}
               </time>
             </p>
           </div>
           <div>
-            <LikeButton />
+            <form
+              action={togglePostLikeStatus.bind(null, post.id)}
+              className={post.isLiked ? "liked" : ""}
+            >
+              <LikeButton />
+            </form>
           </div>
         </header>
         <p>{post.content}</p>
