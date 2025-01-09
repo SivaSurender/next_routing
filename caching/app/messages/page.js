8084845ea96c885d2bbd3b292cdfa407f2d9fee4
bala_e <e.surender14@gmail.com>
2025-01-09 14:22:05 +0530

    Refactor message fetching to remove unnecessary headers and implement caching with revalidation

--- caching/app/messages/page.js
+++ caching/app/messages/page.js
@@ -1,9 +1,13 @@
-import Messages from '@/components/messages';
+import Messages from "@/components/messages";
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
+  const response = await fetch("http://localhost:8080/messages", {
+    // force-cache default for nextjs
+    // default for next 15 - no-store
+    // cache: "no-store",
+    next: {
+      // 5 seconds
+      revalidate: 5,
     },
   });
   const messages = await response.json();
