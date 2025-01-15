86353d82147bbeca4089ffa68f87679536f7019e
bala_e <e.surender14@gmail.com>
2025-01-15 17:10:32 +0530

    Add event listing components and styles for event display

---
+++ page-based-routing/components/events/event-list.js
@@ -0,0 +1,23 @@
+import EventItem from "./event-item";
+import classes from "./event-item.module.css";
+
+function EventList({ events }) {
+  return (
+    <ul className={classes.list}>
+      {events.map((event) => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default EventList;
+{
+}
--- page-based-routing/pages/index.js
+++ page-based-routing/pages/index.js
@@ -1,3 +1,11 @@
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../dummy-data";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const featuredEvents = getFeaturedEvents();
+  return (
+    <div>
+      <EventList events={featuredEvents} />
+    </div>
+  );
 }
