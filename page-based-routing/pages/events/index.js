adf53874abe1b1eeccb61f7effc5969ef2aaac3e
bala_e <e.surender14@gmail.com>
2025-01-16 12:28:38 +0530

    Add event search functionality to filter events by year and month

--- page-based-routing/pages/events/index.js
+++ page-based-routing/pages/events/index.js
@@ -1,12 +1,18 @@
+import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../dummy-data";
 
 export default function Events() {
   const allEvents = getAllEvents();
+  const router = useRouter();
+  function findEventsHandle(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
   return (
     <>
-      <EventSearch />
+      <EventSearch onSearch={findEventsHandle} />
       <EventList events={allEvents} />
     </>
   );
